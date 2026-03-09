2f4cd78115cec8720d1d0ab525e757efb717674b
Kade McRae <kadejmcrae@outlook.com>
2026-03-09 15:20:30 -0700

    Rewrote tests and fixed work on StatCalculation

--- Assets/tst/kade/Tests/EditTests/statBoundaryTest.cs
+++ Assets/tst/kade/Tests/EditTests/statBoundaryTest.cs
@@ -1,11 +1,10 @@
-using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
+[TestFixture]
+[Category("Boundary")]
 public class riskTest
 {
-    // A Test behaves as an ordinary method
     [Test]
     [TestCase(99, false)]
     [TestCase(100, true)]
@@ -13,12 +12,11 @@ public class riskTest
     public void Risk_atThreshold_TriggerGameOver(int finalRisk, bool shouldEnd)
     {
         GameObject go = new GameObject();
-        var riskCalc = go.AddComponent<RiskCalculation>();
+        var riskCalc = go.AddComponent<ArrestStats>();
         riskCalc.riskVal = finalRisk;
 
         bool isGameOver = riskCalc.riskVal >= 100;
 
         Assert.AreEqual(shouldEnd, isGameOver, $"Risk of {finalRisk} should have resulted in GameOver: {shouldEnd}");
-
     }
 }
