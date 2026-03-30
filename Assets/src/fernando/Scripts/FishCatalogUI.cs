818a0243d6a1be67421d3462b564c3887cde8806
fernando <161063412+nimbus333@users.noreply.github.com>
2026-03-30 15:39:23 -0700

    Added database

--- Assets/src/fernando/Scripts/FishCatalogUI.cs
+++ Assets/src/fernando/Scripts/FishCatalogUI.cs
@@ -3,10 +3,22 @@ using UnityEngine;
 public class FishCatalogUI : MonoBehaviour
 {
     public GameObject RT;
+    public GameObject RM;
+    public GameObject RB;
+    public GameObject LT;
+
+    public GameObject LM;
+    public GameObject LB;
+
 
     void Start()
     {
         RT.SetActive(false);
+        RM.SetActive(false);
+        RM.SetActive(false);
+        LT.SetActive(false);
+        LM.SetActive(false);
+        LB.SetActive(false);
 
         for (int i = 0; i < FishDatabaseManager.Instance.fishDatabase.Count; i++)
         {
@@ -15,6 +27,11 @@ public class FishCatalogUI : MonoBehaviour
             if (fish.fishKnown)
             {
                 RT.SetActive(true);
+                RM.SetActive(true);
+                RM.SetActive(true);
+                LT.SetActive(true);
+                LM.SetActive(true);
+                LB.SetActive(true);
             }
         }
     }
