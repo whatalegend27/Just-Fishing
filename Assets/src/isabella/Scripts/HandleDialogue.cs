4eb63748478fda724fdb6f1122e985bec5925adb
evansi938 <isabellaevans938@gmail.com>
2026-04-01 13:36:52 -0700

    Add in Home Screne interactions

---
+++ Assets/src/isabella/Scripts/HandleDialogue.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HandleDialogue : MonoBehaviour
+{
+    public GameObject DialogueBox;
+    void Start()
+    {
+        DialogueBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
